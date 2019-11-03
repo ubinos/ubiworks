1c44e076a121f11fc1db816f3ece9ec1e0727e41
Sungho Park <siminsungho@gmail.com>
2019-11-03 23:45:41 +0900

    Update library
    * ubinos
        + [UBI-135] Add config option "UBINOS__BSP__USE_SOFTFLOAT"
        + Add config option "UBINOS__BSP__GDBSERVER_HOST", "UBINOS__BSP__GDBSERVER_PORT"
        + Refactoring application "cli_tester"
        + Fix task name display error
        + Support board "NUCLEOF207ZG"
        + Support openocd for STM32 family boards
        + Support STM32 Cube F2
        + Refactoring
        + Support STM3221GEVAL USART6
        + Change cmake variable "UBINOS__BSP__BUSYWAITCOUNT_PER_MS" value of config "stm3221geval_sram"
        + Add support STM3221G-EVAL support 2 (ubinos_stm3221geval)
        + Add support STM3221G-EVAL support (ubinos_stm3221geval_baremetal)
        + Refactoring
        + Modify build script to create compile_flags files
        + Refactoring interrupt handler
    * nrf5sdk
        + Refactoring
        + Add config option "NRF5SDK__ENABLE_APP_UART_FIFO"
    * nrf5example
        + Refactoring

--- make/batch.mk
+++ make/batch.mk
@@ -1,135 +1,302 @@
 ###############################################################################
 
 %:
-	@echo "==============================================================================="
+	@echo ""
 	@echo ""
 
+#######################################
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_sam9xe512ek_baremetal_sram
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_sam9xe512ek_baremetal
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_sam9xe512ek
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_sam9xe512ek_baremetal_sram
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_sam9xe512ek_baremetal
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_sam9xe512ek
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=ubik_tester_sam9xe512ek
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=cli_tester_sam9xe512ek
+	@echo ""
 	@echo ""
 
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_baremetal_sram
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_baremetal
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_baremetal_trace
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_trace
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_softdevice_baremetal
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_softdevice_baremetal_trace
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_softdevice
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nrf52dk_softdevice_trace
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_nrf52dk_baremetal_sram
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_nrf52dk_baremetal
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=ubik_tester_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=cli_tester_nrf52dk
+	@echo ""
 	@echo ""
 
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_stm3221geval_baremetal_sram
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_stm3221geval_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_stm3221geval
+	@echo ""
+	@echo ""
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_stm3221geval_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_stm3221geval_baremetal_sram
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_stm3221geval
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=ubik_tester_stm3221geval
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=cli_tester_stm3221geval
+	@echo ""
+	@echo ""
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nucleof207zg_baremetal_sram
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nucleof207zg_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/config CONFIG_NAME=ubinos_nucleof207zg
+	@echo ""
+	@echo ""
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_nucleof207zg_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_nucleof207zg_baremetal_sram
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=helloworld_mt_nucleof207zg
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=ubik_tester_nucleof207zg
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/ubinos/app CONFIG_NAME=cli_tester_nucleof207zg
+	@echo ""
+	@echo ""
+
+#######################################
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk/config CONFIG_NAME=nrf5sdk_nrf52dk_baremetal
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
 	@echo ""
 
+#######################################
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myrtc2_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=mycli2_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_blinky2_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_blinky2_c_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_multilink_central2_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_att_mtu_throughput2_nrf52dk
 	@echo ""
-	@echo "-------------------------------------------------------------------------------"
 	@echo ""
 
+#######################################
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
-	@echo "==============================================================================="
 
 ###############################################################################
 
