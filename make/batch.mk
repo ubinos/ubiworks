5bb5c7237f34c424fdcbd0924698ba70dc5e99c0
Sungho Park <siminsungho@gmail.com>
2019-11-04 10:24:31 +0900

    Add library "stm32cubef2", "stm32f2example"

--- make/batch.mk
+++ make/batch.mk
@@ -297,6 +297,36 @@
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/stm32cubef2/config CONFIG_NAME=stm32cubef2_stm3221geval_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/stm32cubef2/config CONFIG_NAME=stm32cubef2_nucleof207zg_baremetal
+	@echo ""
+	@echo ""
+
+#######################################
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/stm32f2example/app CONFIG_NAME=myuart_printf_stm3221geval
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/stm32f2example/app CONFIG_NAME=mygpio_exti_stm3221geval
+	@echo ""
+	@echo ""
+
+#######################################
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 
 ###############################################################################
 
