8e82318c04eff83bd08e0d5dd33b0eda806cfd35
Sungho Park <siminsungho@gmail.com>
2020-07-10 00:00:57 +0900

    add libraries, nrf5sdk_wrapper, nrf5sdk_v17.00.00_lite, stm32cubef2_wrapper, stm32cubef2_v01.09.00_lite, and remove libraries, nrf5sdk, nrf5sdkexample, stm32cubef2, stm32f2example

--- make/makefile.mk
+++ make/makefile.mk
@@ -4,17 +4,13 @@ LIBRARY_DIR ?= ../library
 include $(LIBRARY_DIR)/ubinos/make/common.mk
 -include $(LIBRARY_DIR)/ubinos/make/custom.mk
 
-help config menuconfig xconfig build clean cleand doc all rebuild rebuildd load reset run debug attach:
+help config menuconfig xconfig build clean cleand doc all rebuild rebuildd load reset run debug attach env test:
 	make -C $(CONFIG_DIR)/../make -f makefile.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) CONFIG_DIR=$(realpath $(CONFIG_DIR)) CONFIG_NAME=$(CONFIG_NAME) $@
 
 zbatch-%:
-	make -C ../library/ubinos/make -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+	make -C ../library/ubinos/make                      -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+	make -C ../library/seggerrtt_wrapper/make           -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+	make -C ../library/nrf5sdk_wrapper/make             -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+	make -C ../library/stm32cubef2_wrapper/make         -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
 
-	make -C ../library/nrf5sdk/make -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/nrf5example/make -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	
-	make -C ../library/stm32cubef2/make -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/stm32f2example/make -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-
-	make -C ../library/seggerrtt_wrapper/make -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
 
