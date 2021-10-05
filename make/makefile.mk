4743cd14913b27004a51f2563cce93380b868874
Sungho Park <siminsungho@gmail.com>
2021-10-05 15:35:04 +0900

    update libraries

--- make/makefile.mk
+++ make/makefile.mk
@@ -4,14 +4,18 @@ LIBRARY_DIR ?= ../library
 include $(LIBRARY_DIR)/ubinos/make/common.mk
 -include $(LIBRARY_DIR)/ubinos/make/custom.mk
 
-help config menuconfig xconfig build clean cleand doc all rebuild rebuildd load reset run debug attach env test:
+help all config configd build clean cleand rebuild rebuildd debugs load run reset debug attach env cleanenv xconfig menuconfig doc test:
 	make -C $(CONFIG_DIR)/../make -f makefile.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) CONFIG_DIR=$(realpath $(CONFIG_DIR)) CONFIG_NAME=$(CONFIG_NAME) $@
+
+xsel:
+	python3 "$(_UBINOS_DIR)/make/confsel.py" .. library
 
 zbatch-%:
 	make -C ../library/ubinos/make                      -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
 	make -C ../library/seggerrtt_wrapper/make           -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
 	make -C ../library/nrf5sdk_wrapper/make             -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+	make -C ../library/nrf5sdk_extension/make           -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
 	make -C ../library/stm32cubef2_wrapper/make         -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/esp8266at/make                   -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-
+	make -C ../library/stm32cubef2_extension/make       -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+#	make -C ../library/esp8266at/make                   -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
 
