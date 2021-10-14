f25428a869ee11723a7bd79feb8682bbce0e03f7
Sungho Park <siminsungho@gmail.com>
2021-10-14 16:10:05 +0900

    update ubinos build scripts

--- make/makefile.mk
+++ make/makefile.mk
@@ -1,23 +1,19 @@
-OUTPUT_BASE_DIR ?= ..
 LIBRARY_DIR ?= ../library
+OUTPUT_BASE_DIR ?= ..
+
+help: common-help
 
 include $(LIBRARY_DIR)/ubinos/make/common.mk
 -include $(LIBRARY_DIR)/ubinos/make/custom.mk
 
-help all config configd build clean cleand rebuild rebuildd debugs load run reset debug attach env cleanenv xconfig menuconfig doc test:
-	make -C $(CONFIG_DIR)/../make -f makefile.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) CONFIG_DIR=$(realpath $(CONFIG_DIR)) CONFIG_NAME=$(CONFIG_NAME) $@
+%: common-% ;
 
-xsel:
-	python3 "$(_UBINOS_DIR)/make/confsel.py" .. library
-
-zbatch-%:
-	make -C ../library/ubinos/make                      -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/CMSIS_5_wrapper/make             -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/seggerrtt_wrapper/make           -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/tflite-micro_wrapper/make        -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/nrf5sdk_wrapper/make             -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/nrf5sdk_extension/make           -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/stm32cubef2_wrapper/make         -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-	make -C ../library/stm32cubef2_extension/make       -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
-#	make -C ../library/esp8266at/make                   -f batch.mk OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $*
+zbatch-%: common-zbatch-%
+	make -C ../library/ubinos/make            	                OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
+	make -C ../library/CMSIS_5_wrapper/make                     OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
+	make -C ../library/seggerrtt_wrapper/make                   OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
+	make -C ../library/nrf5sdk_wrapper/make                     OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
+	make -C ../library/stm32cubef2_wrapper/make                 OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
+	make -C ../library/tflite-micro_wrapper/make                OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
+	# make -C ../library/esp8266at/make                           OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) $@
 
