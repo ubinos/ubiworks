d4a648fbfcce918cc9bc61e86a4487eb64bdb1e6
Sungho Park <siminsungho@gmail.com>
2022-12-12 22:43:25 +0900

    update for tflite version 2.9.3.

--- make/makefile.mk
+++ make/makefile.mk
@@ -1,7 +1,7 @@
 LIBRARY_DIR ?= ../library
 OUTPUT_BASE_DIR ?= ..
 
-LIBRARY_UPGRADE_LIST ?= ubinos seggerrtt seggerrtt_wrapper CMSIS_5 CMSIS_5_wrapper nrf5sdk_wrapper nrf5sdk_extension nrf5sdk_v17.00.00_lite stm32cubef2_wrapper stm32cubef2_extension stm32cubef2_v01.09.00_lite esp8266at tflite-micro_wrapper tflite-micro ArduinoCore-API_wrapper ArduinoCore-API Adafruit_BusIO_wrapper Adafruit_BusIO Adafruit_Sensor_wrapper Adafruit_Sensor Arduino_LSM9DS1_wrapper Arduino_LSM9DS1 Adafruit_BME280_Library_wrapper Adafruit_BME280_Library DHT-sensor-library_wrapper DHT-sensor-library libcrc libcrc_wrapper jansson jansson_wrapper mp_course_examples
+LIBRARY_UPGRADE_LIST ?= ubinos seggerrtt seggerrtt_wrapper CMSIS_5 CMSIS_5_wrapper nrf5sdk_wrapper nrf5sdk_extension nrf5sdk_v17.00.00_lite stm32cubef2_wrapper stm32cubef2_extension stm32cubef2_v01.09.00_lite esp8266at tflite-micro_wrapper tflite-micro ArduinoCore-API_wrapper ArduinoCore-API Adafruit_BusIO_wrapper Adafruit_BusIO Adafruit_Sensor_wrapper Adafruit_Sensor Arduino_LSM9DS1_wrapper Arduino_LSM9DS1 Adafruit_BME280_Library_wrapper Adafruit_BME280_Library DHT-sensor-library_wrapper DHT-sensor-library libcrc libcrc_wrapper jansson jansson_wrapper mp_course_examples kissfft flatbuffers gemmlowp ruy
 
 help: common-help
 
