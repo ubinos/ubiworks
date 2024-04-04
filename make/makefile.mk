e3f47dcda569ec55eda6bb7b6f8781b11a83ad07
Sungho Park <siminsungho@gmail.com>
2024-04-04 09:41:10 +0900

    Change make target name, opendoc to xopendoc.

--- make/makefile.mk
+++ make/makefile.mk
@@ -11,7 +11,7 @@ include $(LIBRARY_DIR)/ubinos/make/common.mk
 ifeq ("$(wildcard $(CONFIG_DIR)/$(CONFIG_NAME).cmake)","") # if $(CONFIG_DIR)/$(CONFIG_NAME).cmake is not exist then
 ifneq ("$(wildcard $(CONFIG_DIR)/$(CONFIG_NAME).mk)","")# if $(CONFIG_DIR)/$(CONFIG_NAME).mk is exist then
 BUILD_TYPE_MAKE__MAKE_DIR = $(shell python "$(_TOOLBOX)" get_make_dir_from_config_file $(CONFIG_DIR)/$(CONFIG_NAME).mk)
-all config configd build clean cleand rebuild rebuildd dserver xdserver load reset run xrun debug xdebug attach xattach xconfig menuconfig doc opendoc cleandoc test env cleanenv :
+all config configd build clean cleand rebuild rebuildd dserver xdserver load reset run xrun debug xdebug attach xattach xconfig menuconfig doc xopendoc cleandoc test env cleanenv :
 ifneq ($(strip $(BUILD_TYPE_MAKE__MAKE_DIR)),)
 	make -C ../$(BUILD_TYPE_MAKE__MAKE_DIR) -f makefile.mk         OUTPUT_BASE_DIR=$(realpath $(OUTPUT_BASE_DIR)) LIBRARY_DIR=$(realpath $(LIBRARY_DIR)) CONFIG_DIR=$(realpath $(CONFIG_DIR)) CONFIG_NAME=$(CONFIG_NAME) DEBUG_SERVER_SERIAL=$(DEBUG_SERVER_SERIAL) DEBUG_SERVER_PORT=$(DEBUG_SERVER_PORT) $@
 endif
