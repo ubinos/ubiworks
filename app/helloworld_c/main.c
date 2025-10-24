baf2eaf3d17699e129dba96c1d6aac669f885f3f
Sungho Park <siminsungho@gmail.com>
2025-10-24 17:00:51 +0900

    Refactoring

--- app/helloworld_c/main.c
+++ app/helloworld_c/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
+    printf("\n\n");
     printf("Hello, World from C!\n");
+    printf("\n\n");
     return 0;
 }
--- app/helloworld_cpp/main.cpp
+++ app/helloworld_cpp/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
+    std::cout << std::endl << std::endl;
     std::cout << "Hello, World from C++!" << std::endl;
+    std::cout << std::endl << std::endl;
     return 0;
 }
